e4a9747450c939e82a8a1e6de22c4baba4d82b15
Luca <gabadadzeluca@gmail.com>
2022-12-25 23:51:16 +0400

    empty comit

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ fetch('data.json')
   .then(data => {
     let comments = data.comments;
     addComments(comments);
+    //display current user
+    displayCurrentUser(data.currentUser);
   });
+
 let commentHTML = '';
 function addComments(comments){
     for(let i = 0; i<comments.length; i++){
@@ -15,86 +18,20 @@ function addComments(comments){
 }
 
 
-// function createCommentDiv(comment){
-
-//   //comment thread container 
-//   const commentThread = document.createElement('div');
-//   commentThread.classList.add("thread");
-//   // create comment div
-//   const commentDiv = document.createElement('div');
-//   commentDiv.classList.add('comment');
-
-//   // add image div
-//   const imageDiv = document.createElement('div');
-//   const img = document.createElement('img');
-//   img.src = comment.user.image.webp;
-//   imageDiv.appendChild(img);
-
-//   // date div
-//   const date = document.createElement('div');
-//   date.classList.add('date-posted');
-//   date.innerHTML = comment.createdAt;
-
-//   // inline div of a container
-//   const divHeader = document.createElement('div');
-//   divHeader.classList.add('div-header');
-//   divHeader.appendChild(imageDiv);
-//   divHeader.appendChild(date);
-
-//   // content div
-//   const contentDiv = document.createElement('div');
-//   contentDiv.classList.add('content');
-//   const replyToSpan = document.createElement('span');
-//   replyToSpan.classList.add('tag');
-//   // replyToSpan.innerHTML = comment.replyingTo;
-//   const content = document.createElement('div');
-//   content.innerHTML = comment.content;
-//   contentDiv.appendChild(replyToSpan);
-//   contentDiv.appendChild(content);
-
-//   commentDiv.appendChild(divHeader);
-//   commentDiv.appendChild(contentDiv);
-
-
-  
-//   //display score 
-//   const scoreDiv = document.createElement('div');
-//   const plus = document.createElement('p');
-//   plus.innerHTML = "+";
-//   plus.classList.add('plus');
-//   const minus = document.createElement('p');
-//   minus.innerHTML = "-"
-//   minus.classList.add('minus');
-//   const score = document.createElement('p');
-//   score.innerHTML = comment.score;
-
-//   scoreDiv.appendChild(plus);
-//   scoreDiv.appendChild(score);
-//   scoreDiv.appendChild(minus);
-
-//   commentDiv.appendChild(scoreDiv);
-//   document.querySelector('main').appendChild(commentThread);
-//   commentThread.appendChild(commentDiv);
-  
-//   // reply btn
-//   const replyBtn = document.createElement('p');
-//   replyBtn.innerHTML = 'Reply';
-//   replyBtn.classList.add('reply-btn');
-//   commentDiv.appendChild(replyBtn);
-
-//   // displayReplies(comment, commentThread);
-// }
-
-
-
 function createCommentHTML(commentData, isReply) {
-  console.log(":",commentData.user.username);
   // Build the HTML for the comment
   let commentHTML = '';
   if (!isReply) {
     commentHTML += '<div class="comment-thread">';
   }
-  commentHTML += '<div class="comment">';
+  let className;
+  if(isReply){
+    className = 'reply';
+  }else{
+    className = 'comment';
+  }
+  
+  commentHTML += `<div class=${className}>`;
 
   // open inline div
   commentHTML += '<div class="inline">';
@@ -115,10 +52,10 @@ function createCommentHTML(commentData, isReply) {
   commentHTML += '<div class="comment-content">' + `<p>${replyDiv}${commentData.content}</p>` + '</div>';
 
   // score
-  commentHTML += '<div class="score">' + `<p class="plus">+</p><p>${commentData.score}</p><p>-</p>` + '</div>';
+  commentHTML += '<div class="score">' + `<p class="plus">+</p><p>${commentData.score}</p><p class="minus">-</p>` + '</div>';
 
   // reply button
-  commentHTML += '<div class="reply">' + 'Reply' + '</div>';
+  commentHTML += '<div class="replyBtn">' + 'Reply' + '</div>';
 
 
 
@@ -139,3 +76,9 @@ function createCommentHTML(commentData, isReply) {
   return commentHTML;
 }
 
+function displayCurrentUser(userData){
+  const currentImg = document.querySelector('footer img');
+  currentImg.src = userData.image.webp;
+  const currentUsername = userData.username;
+  console.log("logged in as "+currentUsername);
+}
