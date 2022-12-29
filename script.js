7e8ccf303c8b5af36bd358416322cd86ad249285
Luca <gabadadzeluca@gmail.com>
2022-12-29 10:47:50 +0400

    add closing tag to the cointainer

--- script.js
+++ script.js
@@ -124,6 +124,9 @@ function createCommentHTML(commentData, isReply) {
     });
     // commentHTML += '</div>';
   }
+  commentHTML += '</div>';  
+  
+  // close the container
   commentHTML += '</div>';
 
   if (!isReply) {
